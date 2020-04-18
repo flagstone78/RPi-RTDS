7438051d1ede7fca0bb510defb96a29b5e7d2f0e
spencer <mathiaspencer@gmail.com>
2020-04-17 18:42:40 -0600

    Update test1Spec.js
    for cribbage

--- spec/test1Spec.js
+++ spec/test1Spec.js
@@ -102,8 +102,7 @@ function updateGameplayers(){
 describe("tableTally",()=>{
 	let players = [defaultUserData(), defaultUserData()];
 	var tgame = new game(players, {message, updateUsers,updateGameplayers, sendCards,  all: []})
-	console.log("ttt",tgame);
-	
+	//console.log("ttt",tgame);
 
 	describe("matching",()=>{
 		it("counts single", ()=>{
@@ -181,6 +180,77 @@ describe("tableTally",()=>{
 							}
 							//console.log(i+j*13,[i+k*13]);
 						}
+					}
+				}
+			}
+		})
+	})
+	xdescribe("adds to 15",()=>{
+		it("counts 7,8 as 2",()=>{
+
+		})
+	})
+
+});
+describe('handTally',()=>{
+	let players = [defaultUserData(), defaultUserData()];
+	var tgame = new game(players, {message, updateUsers,updateGameplayers, sendCards,  all: []})
+	//console.log(tgame.cards.getProperties(0))
+	describe('knobbs',()=>{
+		it("counts Jack of spades(Js) in hand and Ace of spades(As) in center as 1 point",()=>{
+			tgame.centerCard=0
+			let hand=[10]
+			expect(tgame.knobbs(tgame.centerCard,hand)).toBe(1)
+		})
+
+		it("counts Js spades(s) as 1",()=>{
+			for (let i=0;i<13;i++){
+				tgame.centerCard=i
+				let hand=[10]
+				expect(tgame.knobbs(tgame.centerCard,hand)).toBe(1)
+			}
+		})
+
+		it("counts JJJJ card(c) as 1",()=>{
+			for (let i=0;i<52;i++){
+				tgame.centerCard=i
+				let hand=[10,23,36,49]
+				expect(tgame.knobbs(tgame.centerCard,hand)).toBe(1)
+			}
+		})
+
+		let noJ=Array.from(Array(52).keys())//array of cards without Jacks
+		for (let j=10;j<52;j+=12){
+				noJ.splice(j,1)
+			}
+		//console.log(noJ)
+		it("counts noJnoJnoJnoJ c as 0",()=>{
+			for (let j=3;j<noJ.length;j++){
+				for (let i=0;i<52;i++){
+					tgame.centerCard=i
+					let hand=[noJ[j],noJ[j-1],noJ[j-2],noJ[j-3]]
+					if(tgame.knobbs(tgame.centerCard,hand)){
+						console.log('i=',i)
+						console.log('hand=',hand)
+						//console.log(tgame.knobbs(tgame.centerCard,hand))
+					}
+					expect(tgame.knobbs(tgame.centerCard,hand)).toBe(0)
+				}
+			}
+		})
+		it("counts msJnoJnoJnoJ msC as 1",()=>{
+			for (let i=2;i<noJ.length;i++){
+				for (let j=0;j<13;j++){
+					for (let k=0;k<4;k++){
+						tgame.centerCard=j+k*13
+						let hand=[k*13+10,noJ[i-1],noJ[i-2],noJ[i]]
+						if(tgame.knobbs(tgame.centerCard,hand)==0){
+							console.log('jack',10+k*13)
+							console.log('centerCard',j+k*13)
+							console.log('hand=',hand)
+							//console.log(tgame.knobbs(tgame.centerCard,hand))
+						}
+						expect(tgame.knobbs(tgame.centerCard,hand)).toBe(1)
 					}
 				}
 			}
